4d9479ca76516a50482172180126264c2de7382f
POCCOMAXA <opoccomaxao@gmail.com>
2022-03-15 22:03:27 +0200

    .

--- generics_test.go
+++ generics_test.go
@@ -1,23 +1,23 @@
 package golangtest
 
-// import (
-// 	"fmt"
-// 	"testing"
-// )
+import (
+	"fmt"
+	"testing"
+)
 
-// // The index function returns the index of the first occurrence of v in s,
-// // or -1 if not present.
-// func index[E comparable](s []E, v E) int {
-// 	for i, vs := range s {
-// 		if v == vs {
-// 			return i
-// 		}
-// 	}
-// 	return -1
-// }
+// The index function returns the index of the first occurrence of v in s,
+// or -1 if not present.
+func index[E comparable](s []E, v E) int {
+	for i, vs := range s {
+		if v == vs {
+			return i
+		}
+	}
+	return -1
+}
 
-// func TestGenerics(t *testing.T) {
-// 	s := []int{1, 3, 5, 2, 4}
-// 	fmt.Println(index(s, 3))
-// 	fmt.Println(index(s, 6))
-// }
+func TestGenerics(t *testing.T) {
+	s := []int{1, 3, 5, 2, 4}
+	fmt.Println(index(s, 3))
+	fmt.Println(index(s, 6))
+}
