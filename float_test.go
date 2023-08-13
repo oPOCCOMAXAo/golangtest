b69bd285ea13b6879ef366852d240a015a90bc95
POCCOMAXA <opoccomaxao@gmail.com>
2023-08-14 01:17:20 +0300

    .

--- float_test.go
+++ float_test.go
@@ -4,6 +4,9 @@ import (
 	"math"
 	"strconv"
 	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFloat(t *testing.T) {
@@ -31,4 +34,59 @@ func TestFloat(t *testing.T) {
 	}
 
 	t.Log(input)
+}
+
+func TestFloatToIntCastSaveInt(t *testing.T) {
+	const maxSafeInt = 1<<53 - 1
+	const minSafeInt = -(1<<53 - 1)
+	t.Logf("max: %d, %b", maxSafeInt, maxSafeInt)
+	t.Logf("min: %d, %b", minSafeInt, minSafeInt)
+
+	floats := []float64{
+		float64(math.MaxInt) * 1000,
+		float64(math.MinInt) * 1000,
+	}
+
+	for _, floatVal := range floats {
+		t.Logf("value: %f", floatVal)
+
+		intVal := int(floatVal)
+		t.Logf("raw cast: %d, %b", intVal, intVal)
+
+		intVal = int(math.Round(floatVal))
+		t.Logf("round cast: %d, %b", intVal, intVal)
+
+		intVal = int(lo.Clamp(floatVal, minSafeInt, maxSafeInt))
+		t.Logf("clamp cast: %d, %b", intVal, intVal)
+		assert.Contains(t, []int{minSafeInt, maxSafeInt}, intVal)
+	}
+}
+
+func TestFloatToIntCastOverflow(t *testing.T) {
+	minInt := math.MinInt
+	t.Logf("min: %d, %b", minInt, minInt)
+
+	const maxSafeFloat = 1<<63 - 1<<9 - 1
+	const minSafeFloat = -(1<<63 - 1<<9 - 1)
+	t.Logf("max: %d, %b", maxSafeFloat, maxSafeFloat)
+	t.Logf("min: %d, %b", minSafeFloat, minSafeFloat)
+
+	floats := []float64{
+		float64(math.MaxInt) * 1000,
+		float64(math.MinInt) * 1000,
+	}
+
+	for _, floatVal := range floats {
+		t.Logf("value: %f", floatVal)
+
+		intVal := int(floatVal)
+		t.Logf("raw cast: %d, %b", intVal, intVal)
+
+		intVal = int(math.Round(floatVal))
+		t.Logf("round cast: %d, %b", intVal, intVal)
+
+		intVal = int(lo.Clamp(floatVal, minSafeFloat, maxSafeFloat))
+		t.Logf("clamp cast: %d, %b", intVal, intVal)
+		assert.NotEqual(t, math.MinInt, intVal)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,15 @@ go 1.18
 
 require (
 	github.com/opoccomaxao-go/event/v3 v3.0.0
+	github.com/opoccomaxao-go/reactive-vars v0.0.0-20220608105521-0465beab2cef
+	github.com/pkg/errors v0.9.1
+	github.com/samber/lo v1.38.1
 	github.com/stretchr/testify v1.7.1
 )
 
-require golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf // indirect
-
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/opoccomaxao-go/reactive-vars v0.0.0-20220608105521-0465beab2cef
-	github.com/pkg/errors v0.9.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
